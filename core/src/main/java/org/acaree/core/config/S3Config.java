6337420195e3e15d3c15864ee7da6fc155fd05c3
Chris Godwill <chrisgodswill115@gmail.com>
2024-03-19 09:02:48 -0400

    updated WebMain

--- core/src/main/java/org/acaree/core/config/S3Config.java
+++ core/src/main/java/org/acaree/core/config/S3Config.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
--- web/src/main/java/org/acaree/WebMain.java
+++ web/src/main/java/org/acaree/WebMain.java
@@ -1,11 +1,15 @@
 package org.acaree;
 
+import org.acaree.core.config.AppConfig;
+import org.acaree.core.config.S3Config;
 import org.acaree.core.repository.PersonRepository;
 import org.acaree.core.service.PersonService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -13,6 +17,7 @@ import java.io.File;
 import java.util.Arrays;
 
 @SpringBootApplication
+@Import({S3Config.class})
 public class WebMain {
 
     public static void main(String[] args) {
