c466322b7c7c05896b1f7cf84f7c4c11b1698b60
Chris Godwill <chrisgodswill115@gmail.com>
2024-03-21 15:37:00 -0400

    updated WebConfig file for CORS

--- core/src/main/java/org/acaree/core/config/WebConfig.java
+++ core/src/main/java/org/acaree/core/config/WebConfig.java
@@ -6,16 +6,23 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply CORS to all paths
-                .allowedOrigins("http://theacaree.com",
-                        "http://theacaree.com.s3-website-us-east-1.amazonaws.com",
-                        "http://localhost:3000") // Allow  frontend domain
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
-                .allowedHeaders("*")
-                .maxAge(3600); // Maximum age of CORS preflight (in seconds)
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Apply CORS to all paths under /api
+                        .allowedOrigins(
+                                "http://theacaree.com.s3-website-us-east-1.amazonaws.com",
+                                "https://theacaree.com",
+                                "http://localhost:3000" // Allow requests from these origins
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allows all headers
+                        .allowCredentials(true); // Allow cookies
+            }
+        };
     }
 }
