5e2efcd3f2da8d3fbeac2cc6eb13ad0778c5535b
Chris Godwill <chrisgodswill115@gmail.com>
2024-03-24 05:51:49 -0400

    updated core POM

--- core/src/main/java/org/acaree/core/config/S3Config.java
+++ core/src/main/java/org/acaree/core/config/S3Config.java
@@ -5,9 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -26,7 +23,6 @@ public class S3Config {
     public S3Client s3Client(){
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
