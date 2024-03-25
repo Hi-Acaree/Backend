8253dfc9332153eefc1825cfd866fbd864c74164
Chris Godwill <chrisgodswill115@gmail.com>
2024-03-24 21:42:49 -0400

    Added logging for aws for debugging

--- core/src/main/java/org/acaree/core/config/S3Config.java
+++ core/src/main/java/org/acaree/core/config/S3Config.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -22,6 +23,7 @@ public class S3Config {
     @Bean
     public S3Client s3Client(){
         return S3Client.builder()
+                .credentialsProvider(InstanceProfileCredentialsProvider.create())
                 .region(Region.of(region))
                 .build();
     }
